c361464abf586773896bbaa638121d95526728f8
Michelle Frattaroli <mishyjari@outlook.com>
2020-04-01 18:39:55 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -17,38 +17,32 @@ class Song
     @artist = artist
     @genre = genre
     
+    # Iterate counter and push args into class arrays
     @@count += 1
     @@genres.push(genre)
     @@artists.push(artist)
     
-    if !@@artist_count[artist]
-      @@artist_count[artist] = 0
-    end
+    # Create genre and artist keys in count hashes if not exist then iterate
+    @@artist_count[artist] = 0 if !@@artist_count[artist]
     @@artist_count[artist] += 1
     
-    if !@@genre_count[genre]
-      @@genre_count[genre] = 0
-    end
+    @@genre_count[genre] = 0 if !@@genre_count[genre]
     @@genre_count[genre] += 1
-    
   end
   
+  # getters
   def self.count
     @@count
   end
-  
   def self.artists
-    @@artists.uniq
+    @@artists.uniq # uniq() method to filter duplicates
   end
-  
   def self.genres
     @@genres.uniq
   end
-  
   def self.artist_count
     @@artist_count
   end
-  
   def self.genre_count
     @@genre_count
   end
